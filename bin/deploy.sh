081404df207e356f59faa1320d14484c903e192e
Vadim <eugene.s@zpoken.ai>
2023-03-26 21:29:23 +0300

    xm

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,7 +5,8 @@ if [[ $1 = "prod" || $1 = "dev" ]] && [[ $2 = "down" || $2 = "up" ]]; then
     fileEnv="docker-compose.${1}.yaml"
     downOrUp=$2
     echo "Running docker-compose -f docker-compose.yaml -f $fileEnv $downOrUp"
-    docker build -t client-prod-i docker-compose -f docker-compose.yaml -f $fileEnv $downOrUp
+    docker build -t client-prod-i .
+    docker-compose -f docker-compose.yaml -f $fileEnv $downOrUp
 else
     echo 'Need to follow format ./deploy prod|dev down|up'
 fi
