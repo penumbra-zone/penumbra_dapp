56af0d349d5120763b0668c317614f81600803c0
Vadim <eugene.s@zpoken.ai>
2023-03-26 23:46:00 +0300

    asd

--- webpack.development.js
+++ webpack.development.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
     },
     compress: true,
     open: true,
-    // public: 'penumbradapp.zpoken.io', 
+    allowedHosts: ['penumbradapp.zpoken.io'],
     static: './build',
     port: 9012,
   },
