c2e80709b9c38651ddad79bc4f49307ae9d364f5
Vadim <eugene.s@zpoken.ai>
2023-05-24 14:27:24 +0300

    design changes

--- src/components/ModalWrapper/index.tsx
+++ src/components/ModalWrapper/index.tsx
@@ -26,27 +26,92 @@ export const ModalWrapper: React.FC<ModalWrapperPropsType> = ({
 		e.stopPropagation()
 
 	const modalContent = show ? (
+		// <div
+		// 	className='top-[0] w-[100%] h-[100vh] fixed bg-background-0.7 overflow-y-hidden z-150 flex items-center justify-center'
+		// 	onClick={onClose}
+		// >
+		// 	{/* <div className='flex items-center justify-center'> */}
+		// 		<div
+		// 			className={`w-[100%] overflow-auto flex ${
+		// 				position === 'center'
+		// 					? 'items-center justify-center'
+		// 					: 'items-start justify-end mt-[100px] ext:mx-[40px] laptop:mx-[312px]'
+		// 			}`}
+		// 		>
+		// 			<div
+		// 				className={`bg-brown rounded-[15px] p-[30px] ${className}`}
+		// 				onClick={stopPropagation}
+		// 			>
+		// 				{children}
+		// 			</div>
+		// 		</div>
+		// 	{/* </div> */}
+		// </div>
 		<div
-			className='top-[0] w-[100%] h-[100vh] fixed bg-background-0.7 overflow-y-hidden z-150 flex items-center justify-center'
-			onClick={onClose}
-		>
-			{/* <div className='flex items-center justify-center'> */}
-				<div
-					className={`w-[100%] ext:h-[100vh] tablet:h-[80vh] overflow-auto flex ${
-						position === 'center'
-							? 'items-center justify-center'
-							: 'items-start justify-end mt-[100px] ext:mx-[40px] laptop:mx-[312px]'
-					}`}
-				>
-					<div
-						className={`bg-brown rounded-[15px] p-[30px] ${className}`}
-						onClick={stopPropagation}
-					>
-						{children}
-					</div>
-				</div>
-			{/* </div> */}
+		data-te-modal-init
+		className="fixed left-0 top-0 z-[1055]  h-full w-full overflow-y-auto overflow-x-hidden outline-none bg-background-0.7"
+		id="exampleModalScrollable"
+		tabIndex={-1}
+		aria-labelledby="exampleModalScrollableLabel"
+		aria-hidden="true"
+		onClick={onClose}>
+		<div
+			data-te-modal-dialog-ref
+			className="pointer-events-none relative flex items-center w-auto translate-y-[-50px]  transition-all duration-300 ease-in-out mx-auto mt-7 h-[calc(100%-3.5rem)] max-w-[500px]">
+			<div
+				className="pointer-events-auto relative flex max-h-[75vh] w-full flex-col overflow-hidden rounded-[15px] bg-brown outline-none">
+				{/* <div
+					className="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+					<h5
+						className="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200"
+						id="exampleModalScrollableLabel">
+						Modal title
+					</h5>
+					<button
+						type="button"
+						className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+						data-te-modal-dismiss
+						aria-label="Close">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							className="h-6 w-6">
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					</button>
+				</div> */}
+	
+				{/* <!--Modal body--> */}
+				{children}
+	
+				{/* <!--Modal footer--> */}
+				{/* <div
+					className="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+					<button
+						type="button"
+						className="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
+						data-te-modal-dismiss
+						data-te-ripple-init
+						data-te-ripple-color="light">
+						Close
+					</button>
+					<button
+						type="button"
+						className="ml-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+						data-te-ripple-init
+						data-te-ripple-color="light">
+						Save changes
+					</button>
+				</div> */}
+			</div>
 		</div>
+	</div>
 	) : null
 
 	if (_document) {
