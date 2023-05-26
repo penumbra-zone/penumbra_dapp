3167cb69c54f101c515628444dbb8512a8a2f6b4
Vadim <eugene.s@zpoken.ai>
2023-05-26 13:03:30 +0300

    Merge branch 'main' of github.com:penumbra-zone/penumbra_dapp

--- src/components/AssetsList/index.tsx
+++ src/components/AssetsList/index.tsx
@@ -18,40 +18,47 @@ export const AssetsList: React.FC<IAssetList> = ({ assets }) => {
 
 	return (
 		<>
-			{Object.entries(assets).filter(i =>Number(i[1].amount!.lo)).map(i => (
-				<div
-					key={i[0]}
-					// onClick={handleBalanceDetail(i[0])}
-					// role='button'
-					// tabIndex={0}
-					className='flex items-center justify-between py-[20px] pl-[22px] pr-[30px] border-y-[1px] border-solid border-dark_grey'
-				>
-					{/* <div className='flex items-center'>
+			{Object.entries(assets)
+				.filter(i => Number(i[1].amount!.lo))
+				.map(i => (
+					<div
+						key={i[0]}
+						// onClick={handleBalanceDetail(i[0])}
+						// role='button'
+						// tabIndex={0}
+						className='flex items-center justify-between py-[20px] pl-[22px] pr-[30px] border-y-[1px] border-solid border-dark_grey'
+					>
+						{/* <div className='flex items-center'>
 			
 						<p className='pl-[16px] text_numbers'>
 							{i[1].denom.denom.includes("nft")?(Number(i[1].amount!.lo)).toLocaleString('en-US'):(Number(i[1].amount!.lo) / 10 ** 6).toLocaleString('en-US')}{' '}
 							{i[1].denom.denom}
 						</p>
 					</div> */}
-					<div className='flex items-center justify-between'>
-						<p className='pl-[16px] text_numbers'>
-							{i[1].denom.denom.includes("nft")?(Number(i[1].amount!.lo)).toLocaleString('en-US', {
-							 minimumFractionDigits: 2,
-							 maximumFractionDigits: 20,
-							}):(Number(i[1].amount!.lo) / 10 ** 6).toLocaleString('en-US',  {
-								minimumFractionDigits: 2,
-								maximumFractionDigits: 20,
-							 })}
-						</p>
-						<p className='pl-[4px] pr-[16px] h4 break-all'>
-							{i[1].denom.denom}
-						</p>
+						<div className='flex items-center justify-between'>
+							<p className='pl-[16px] text_numbers'>
+								{i[1].denom.denom.includes('nft')
+									? Number(i[1].amount!.lo).toLocaleString('en-US', {
+											minimumFractionDigits: 2,
+											maximumFractionDigits: 20,
+									  })
+									: (Number(i[1].amount!.lo) / 10 ** 6).toLocaleString(
+											'en-US',
+											{
+												minimumFractionDigits: 2,
+												maximumFractionDigits: 20,
+											}
+									  )}
+							</p>
+							<p className='pl-[4px] pr-[16px] h4 break-all'>
+								{i[1].denom.denom}
+							</p>
+						</div>
+						<div className='rotate-180'>
+							<ChevronLeftIcon />
+						</div>
 					</div>
-					<div className='rotate-180'>
-						<ChevronLeftIcon />
-					</div>
-				</div>
-			))}
+				))}
 		</>
 	)
 }
--- src/components/modals/TxDetailModal/index.tsx
+++ src/components/modals/TxDetailModal/index.tsx
@@ -1,193 +1,202 @@
 import {
-    NotesResponse,
-    TransactionInfoResponse,
+	NotesResponse,
+	TransactionInfoResponse,
 } from '@buf/penumbra-zone_penumbra.bufbuild_es/penumbra/view/v1alpha1/view_pb'
-import {ModalWrapper} from '../../ModalWrapper'
-import {uint8ToBase64} from '../../../utils/uint8ToBase64'
-import {useBalance} from '../../../context'
-import {base64_to_bech32} from 'penumbra-wasm'
-import {getDenomByAssetId} from '../../../utils/assets'
-import {useMemo} from 'react'
-import {getShortKey} from '../../../utils/getShortValue'
+import { ModalWrapper } from '../../ModalWrapper'
+import { uint8ToBase64 } from '../../../utils/uint8ToBase64'
+import { useBalance } from '../../../context'
+import { base64_to_bech32 } from 'penumbra-wasm'
+import { getDenomByAssetId } from '../../../utils/assets'
+import { useMemo } from 'react'
+import { getShortKey } from '../../../utils/getShortValue'
 
 type TxDetailModalProps = {
-    show: boolean
-    transaction: TransactionInfoResponse
-    notes: NotesResponse[]
-    onClose: () => void
+	show: boolean
+	transaction: TransactionInfoResponse
+	notes: NotesResponse[]
+	onClose: () => void
 }
 
 export const TxDetailModal: React.FC<TxDetailModalProps> = ({
-                                                                show,
-                                                                notes,
-                                                                transaction,
-                                                                onClose,
-                                                            }) => {
-    const {assets} = useBalance()
+	show,
+	notes,
+	transaction,
+	onClose,
+}) => {
+	const { assets } = useBalance()
 
-    const actionText = useMemo(() => {
-        return transaction.txInfo?.view?.bodyView?.actionViews.map(i => {
-            const type = i.actionView.case
+	const actionText = useMemo(() => {
+		return transaction.txInfo?.view?.bodyView?.actionViews.map(i => {
+			const type = i.actionView.case
 
-            if (type === 'spend') {
-                try {
-                    const nullifier = uint8ToBase64(
-                        i.actionView.value.spendView.value?.spend?.body
-                            ?.nullifier as Uint8Array
-                    )
-                    const note = notes.find(
-                        i =>
-                            uint8ToBase64(i.noteRecord?.nullifier?.inner as Uint8Array) ===
-                            nullifier
-                    )
-                    const assetId = getDenomByAssetId(
-                        assets,
-                        uint8ToBase64(
-                            note?.noteRecord?.note?.value?.assetId?.inner as Uint8Array
-                        )
-                    )
+			if (type === 'spend') {
+				try {
+					const nullifier = uint8ToBase64(
+						i.actionView.value.spendView.value?.spend?.body
+							?.nullifier as Uint8Array
+					)
+					const note = notes.find(
+						i =>
+							uint8ToBase64(i.noteRecord?.nullifier?.inner as Uint8Array) ===
+							nullifier
+					)
+					const assetId = getDenomByAssetId(
+						assets,
+						uint8ToBase64(
+							note?.noteRecord?.note?.value?.assetId?.inner as Uint8Array
+						)
+					)
 
-                    return {
-                        type,
-                        text: `${
-                            Number(note?.noteRecord?.note?.value?.amount?.lo) / 10 ** 6
-                        } ${assetId}`,
-                    }
-                } catch (error) {
-                    return {
-                        type,
-                        text: 'Encrypted',
-                    }
-                }
-            } else if (type === 'output') {
-                try {
-                    const assetId = getDenomByAssetId(
-                        assets,
-                        uint8ToBase64(
-                            //@ts-ignore
-                            i.actionView.value.outputView.value.note.value.valueView.value
-                                .assetId.inner as Uint8Array
-                        )
-                    )
+					return {
+						type,
+						text: `${
+							Number(note?.noteRecord?.note?.value?.amount?.lo) / 10 ** 6
+						} ${assetId}`,
+					}
+				} catch (error) {
+					return {
+						type,
+						text: 'Encrypted',
+					}
+				}
+			} else if (type === 'output') {
+				try {
+					const assetId = getDenomByAssetId(
+						assets,
+						uint8ToBase64(
+							//@ts-ignore
+							i.actionView.value.outputView.value.note.value.valueView.value
+								.assetId.inner as Uint8Array
+						)
+					)
 
-                    const addresView =
-                        //@ts-ignore
-                        i.actionView.value.outputView.value.note.address.addressView
-                    const address = base64_to_bech32(
-                        'penumbrav2t',
-                        uint8ToBase64(addresView.value.address.inner)
-                    )
+					const addresView =
+						//@ts-ignore
+						i.actionView.value.outputView.value.note.address.addressView
+					const address = base64_to_bech32(
+						'penumbrav2t',
+						uint8ToBase64(addresView.value.address.inner)
+					)
 
-                    const amount =
-                        //@ts-ignore
-                        assetId.includes("nft") ? Number(i.actionView.value.outputView.value.note.value.valueView.value.amount.lo) : Number(i.actionView.value.outputView.value.note.value.valueView.value.amount.lo) / 10 ** 6
+					const amount =
+						//@ts-ignore
+						assetId.includes('nft')
+							? Number(
+									//@ts-ignore
+									i.actionView.value.outputView.value.note.value.valueView.value
+										.amount.lo
+							  )
+							: Number(
+									//@ts-ignore
+									i.actionView.value.outputView.value.note.value.valueView.value
+										.amount.lo
+							  ) /
+							  10 ** 6
 
-                    return {
-                        text:
-                            addresView.case === 'opaque'
-                                ? `${amount} ${assetId} to ${getShortKey(
-                                    address
-                                )}`
-                                : `${amount} ${assetId}`,
-                        type: addresView.case === 'opaque' ? 'Send' : 'Receive',
-                    }
-                } catch (error) {
-                    return {
-                        type,
-                        text: 'Encrypted',
-                    }
-                }
-            } else if (type === 'positionOpen') {
-                try {
-                    const asset1 = getDenomByAssetId(
-                        assets,
-                        uint8ToBase64(
-                            i.actionView.value.position?.phi?.pair?.asset1
-                                ?.inner as Uint8Array
-                        )
-                    )
-                    const asset2 = getDenomByAssetId(
-                        assets,
-                        uint8ToBase64(
-                            i.actionView.value.position?.phi?.pair?.asset2
-                                ?.inner as Uint8Array
-                        )
-                    )
-                    return {
-                        text: `Trading Pair: (${asset1}, ${asset2})`,
-                        type,
-                    }
-                } catch (error) {
-                    return {
-                        type,
-                        text: 'Encrypted',
-                    }
-                }
-            } else if (type === 'swap') {
-                try {
-                    const delta1I = Number(
-                        i.actionView.value.swapView.value?.swap?.body?.delta1I?.lo
-                    )
-                    const delta2I =
-                        i.actionView.value.swapView.value?.swap?.body?.delta2I?.lo
+					return {
+						text:
+							addresView.case === 'opaque'
+								? `${amount} ${assetId} to ${getShortKey(address)}`
+								: `${amount} ${assetId}`,
+						type: addresView.case === 'opaque' ? 'Send' : 'Receive',
+					}
+				} catch (error) {
+					return {
+						type,
+						text: 'Encrypted',
+					}
+				}
+			} else if (type === 'positionOpen') {
+				try {
+					const asset1 = getDenomByAssetId(
+						assets,
+						uint8ToBase64(
+							i.actionView.value.position?.phi?.pair?.asset1
+								?.inner as Uint8Array
+						)
+					)
+					const asset2 = getDenomByAssetId(
+						assets,
+						uint8ToBase64(
+							i.actionView.value.position?.phi?.pair?.asset2
+								?.inner as Uint8Array
+						)
+					)
+					return {
+						text: `Trading Pair: (${asset1}, ${asset2})`,
+						type,
+					}
+				} catch (error) {
+					return {
+						type,
+						text: 'Encrypted',
+					}
+				}
+			} else if (type === 'swap') {
+				try {
+					const delta1I = Number(
+						i.actionView.value.swapView.value?.swap?.body?.delta1I?.lo
+					)
+					const delta2I =
+						i.actionView.value.swapView.value?.swap?.body?.delta2I?.lo
 
-                    const asset1 = getDenomByAssetId(
-                        assets,
-                        uint8ToBase64(
-                            i.actionView.value.swapView.value?.swap?.body?.tradingPair?.asset1
-                                ?.inner as Uint8Array
-                        )
-                    )
+					const asset1 = getDenomByAssetId(
+						assets,
+						uint8ToBase64(
+							i.actionView.value.swapView.value?.swap?.body?.tradingPair?.asset1
+								?.inner as Uint8Array
+						)
+					)
 
-                    const asset2 = getDenomByAssetId(
-                        assets,
-                        uint8ToBase64(
-                            i.actionView.value.swapView.value?.swap?.body?.tradingPair?.asset2
-                                ?.inner as Uint8Array
-                        )
-                    )
+					const asset2 = getDenomByAssetId(
+						assets,
+						uint8ToBase64(
+							i.actionView.value.swapView.value?.swap?.body?.tradingPair?.asset2
+								?.inner as Uint8Array
+						)
+					)
 
-                    if (delta1I) {
-                        return {
-                            text: `${Number(delta1I) / 10 ** 6} ${asset1} for ${asset2}`,
-                            type,
-                        }
-                    }
-                    return {
-                        text: `${Number(delta2I) / 10 ** 6} ${asset2} for ${asset1}`,
-                        type,
-                    }
-                } catch (error) {
-                    return {
-                        type,
-                        text: 'Encrypted',
-                    }
-                }
-            } else {
-                return {
-                    text: '',
-                    type,
-                }
-            }
-        })
-    }, [transaction, assets, notes])
+					if (delta1I) {
+						return {
+							text: `${Number(delta1I) / 10 ** 6} ${asset1} for ${asset2}`,
+							type,
+						}
+					}
+					return {
+						text: `${Number(delta2I) / 10 ** 6} ${asset2} for ${asset1}`,
+						type,
+					}
+				} catch (error) {
+					return {
+						type,
+						text: 'Encrypted',
+					}
+				}
+			} else {
+				return {
+					text: '',
+					type,
+				}
+			}
+		})
+	}, [transaction, assets, notes])
 
-    return (
-        <ModalWrapper show={show} onClose={onClose}>
-            <div className='relative overflow-y-auto pt-[30px] pb-[52px] px-[24px]'>
-                <div className='w-[100%]'>
-                    {actionText!.map((i, index) => {
-                        return (
-                            <div key={index} className='w-[100%] flex flex-col mt-[16px]'>
-                                <p className='h2 mb-[8px] capitalize'>{i.type}</p>
-                                <p className='py-[8px] px-[16px] bg-dark_grey rounded-[15px] text_numbers_s text-light_grey break-words '>
-                                    {i.text}
-                                </p>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
-        </ModalWrapper>
-    )
+	return (
+		<ModalWrapper show={show} onClose={onClose}>
+			<div className='relative overflow-y-auto pt-[30px] pb-[52px] px-[24px]'>
+				<div className='w-[100%]'>
+					{actionText!.map((i, index) => {
+						return (
+							<div key={index} className='w-[100%] flex flex-col mt-[16px]'>
+								<p className='h2 mb-[8px] capitalize'>{i.type}</p>
+								<p className='py-[8px] px-[16px] bg-dark_grey rounded-[15px] text_numbers_s text-light_grey break-words '>
+									{i.text}
+								</p>
+							</div>
+						)
+					})}
+				</div>
+			</div>
+		</ModalWrapper>
+	)
 }
