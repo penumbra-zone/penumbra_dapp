e8f92d11b5f8113e198846c681c5148d8f828bd6
Vadim <eugene.s@zpoken.ai>
2023-06-26 09:07:44 +0300

    deleter

--- src/App.tsx
+++ src/App.tsx
@@ -245,18 +245,3 @@ function AuthProvider({ children }: { children: React.ReactNode }) {
 export function useAuth() {
 	return useContext(AuthContext)
 }
-
-function RequireAuth({ children }: { children: JSX.Element }) {
-	let auth = useAuth()
-	let location = useLocation()
-
-	if (!auth.walletAddress) {
-		// Redirect them to the /login page, but save the current location they were
-		// trying to go to when they were redirected. This allows us to send them
-		// along to that page after they login, which is a nicer user experience
-		// than dropping them off on the home page.
-		return <Navigate to={routesPath.HOME} state={{ from: location }} replace />
-	}
-
-	return children
-}
