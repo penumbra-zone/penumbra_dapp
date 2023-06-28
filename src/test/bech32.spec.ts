5d41040495de2e3437bca54d70905a3fad2959bc
Conor Schaefer <conor@penumbralabs.xyz>
2023-06-28 10:59:59 -0700

    bech32 test: working example cases
    Updates the new bech32 test cases with working examples, showing how to
    convert a base64-encoded Penumbra address to byte slice suitable for
    bech32 re-encoding back to a string representation of the address.
    Puzzlingly, the `bech32m` methods don't do what we want, so we use the
    `bech32` methods instead.

--- src/test/bech32.spec.ts
+++ src/test/bech32.spec.ts
@@ -1,21 +1,39 @@
 import {fromBase64} from "@cosmjs/encoding";
-import {bech32m} from "bech32";
+import {bech32} from "bech32";
 import {expect, jest, test, describe, it} from '@jest/globals';
 
 
-describe("bech32", () => {
+describe("penumbraAddress", () => {
 
-    const addressByteArray = fromBase64("uO4xMmm5xcE4F1NMZHSCuLerUIZcABueUXSylcnQqYV7uEW8/lFKAClU7mOm4KSwJEDS9F/ToRMWrY4e3WxEIepSdTtxZU9rLni+cp0cvJ0=");
+    // Most of RPC methods relating to addresses return a base64-encoded string.
+    // In order to confirm we can parse an address correctly, we must:
+    //
+    //   1. Convert a base64 message to an array of uint8s, 80 elements long.
+    //   2. Convert the byte arrays to a readable Penumbra string representation.
+    //   3. Confirm that the Penumbra string representation matches our expectations.
+    //
+    const penumbraAddress = "penumbrav2t1hrhrzvnfh8zuzwqh2dxxgayzhzm6k5yxtsqph8j3wjeftjws4xzhhwz9hnl9zjsq992wucaxuzjtqfzq6t69l5apzvt2mrs7m4kygg022f6nkut9fa4ju797w2w3e0yav3m37k";
+    const penumbraAddressBase64 = "uO4xMmm5xcE4F1NMZHSCuLerUIZcABueUXSylcnQqYV7uEW8/lFKAClU7mOm4KSwJEDS9F/ToRMWrY4e3WxEIepSdTtxZU9rLni+cp0cvJ0=";
+    const penumbraAddressByteArray = fromBase64(penumbraAddressBase64);
+    const penumbraAddressLimit = 146;
 
+    describe("toBytes", () => {
+        it("works", () => {
+            expect(penumbraAddressByteArray.length).toBe(80);
+            penumbraAddressByteArray.forEach((i) => {
+                expect(i).toBeGreaterThanOrEqual(0);
+                expect(i).toBeLessThanOrEqual(255);
+            });
+        });
+    })
 
     describe("toBech32", () => {
         it("works", () => {
-
-            expect(bech32m.encode(
+            expect(bech32.encode(
                 'penumbrav2t',
-                bech32m.toWords(addressByteArray),
-                156
-            )).toEqual("penumbrav2t1hrhrzvnfh8zuzwqh2dxxgayzhzm6k5yxtsqph8j3wjeftjws4xzhhwz9hnl9zjsq992wucaxuzjtqfzq6t69l5apzvt2mrs7m4kygg022f6nkut9fa4ju797w2w3e0yav3m37k");
+                bech32.toWords(penumbraAddressByteArray),
+                penumbraAddressLimit,
+            )).toEqual(penumbraAddress);
         });
     })
 });
