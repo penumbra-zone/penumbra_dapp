16c71631843128a99e90dfb091ea0d1a98d00a21
Henry de Valence <hdevalence@penumbralabs.xyz>
2023-07-01 00:31:34 -0700

    use reactjson component dynamically

--- src/app/tx/page.tsx
+++ src/app/tx/page.tsx
@@ -19,7 +19,11 @@ import { Button } from '@/components/Button'
 import { ChevronLeftIcon, CopySvg } from '@/components/Svg'
 import { getTransactionType } from '@/lib/transactionType'
 
-import ReactJson from '@microlink/react-json-view';
+import dynamic from 'next/dynamic'
+//import ReactJson from '@microlink/react-json-view';
+const DynamicReactJson = dynamic(() => import('@microlink/react-json-view'), {
+	ssr: false // This line is important. It's what prevents server-side rendering.
+})
 
 function truncateHash(hash: string | null, length: number = 6): string {
 	if (hash === null) {
@@ -419,7 +423,7 @@ export default function TransactionDetail() {
 								<p className='h2 mb-[12px] mt-[16px]'>Raw Transaction</p>
 								<div className='flex flex-col p-[16px] gap-y-[16px] w-[800px] bg-brown rounded-[10px]'>
 									<div style={{ overflowX: 'auto', whiteSpace: 'nowrap' }}>
-										<ReactJson src={rawTx}
+										<DynamicReactJson src={rawTx}
 											theme="twilight"
 											name={false}
 											displayDataTypes={false}
@@ -432,7 +436,7 @@ export default function TransactionDetail() {
 								<p className='h2 mb-[12px] mt-[16px]'>Raw View</p>
 								<div className='flex flex-col p-[16px] gap-y-[16px] w-[800px] bg-brown rounded-[10px]'>
 									<div style={{ overflowX: 'auto', whiteSpace: 'nowrap' }}>
-										<ReactJson src={rawView}
+										<DynamicReactJson src={rawView}
 											theme="twilight"
 											name={false}
 											displayDataTypes={false}
