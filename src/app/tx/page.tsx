0a356381b7116a26c8c76b5d11e0157253257365
Vadim <eugene.s@zpoken.ai>
2023-07-10 13:06:03 +0300

    s

--- src/app/tx/page.tsx
+++ src/app/tx/page.tsx
@@ -25,7 +25,7 @@ import { TransactionHashComponent } from '@/components/penumbra/TransactionHash'
 import { ActionViewComponent } from '@/components/penumbra/view/ActionView'
 //import ReactJson from '@microlink/react-json-view';
 const DynamicReactJson = dynamic(() => import('@microlink/react-json-view'), {
-	ssr: false // This line is important. It's what prevents server-side rendering.
+	ssr: false, // This line is important. It's what prevents server-side rendering.
 })
 
 export default function TransactionDetail() {
@@ -44,28 +44,28 @@ export default function TransactionDetail() {
 		//@ts-ignore
 		tx?.txInfo?.view?.bodyView?.memoView?.memoView!
 
-	let memoText = 'Encrypted';
-	let memoSender = 'Encrypted';
-	let memoReturnAddress: Address | undefined = undefined;
+	let memoText = 'Encrypted'
+	let memoSender = 'Encrypted'
+	let memoReturnAddress: Address | undefined = undefined
 	if (memoView?.case == 'visible') {
-		memoText = memoView.value.plaintext!.text;
+		memoText = memoView.value.plaintext!.text
 		memoSender = bech32m.encode(
 			'penumbrav2t',
 			bech32m.toWords(memoView.value.plaintext!.sender!.inner),
 			160
-		);
+		)
 		// https://github.com/penumbra-zone/penumbra/issues/2782
 		memoReturnAddress = memoView.value.plaintext?.sender
 	}
 
-		const chainId = bodyView?.chainId
-		const feeAmount =
-			Number(bodyView?.fee?.amount?.lo) +
-			2 ** 64 * Number(bodyView?.fee?.amount?.hi)
-		const feeText = `${feeAmount} upenumbra`
-		let expiryText = 'None'
-		if (bodyView?.expiryHeight != BigInt(0))
-			expiryText = `${bodyView?.expiryHeight}`
+	const chainId = bodyView?.chainId
+	const feeAmount =
+		Number(bodyView?.fee?.amount?.lo) +
+		2 ** 64 * Number(bodyView?.fee?.amount?.hi)
+	const feeText = `${feeAmount} upenumbra`
+	let expiryText = 'None'
+	if (bodyView?.expiryHeight != BigInt(0))
+		expiryText = `${bodyView?.expiryHeight}`
 
 	useEffect(() => {
 		if (!auth!.walletAddress) return
@@ -93,8 +93,8 @@ export default function TransactionDetail() {
 	//const rawView = tx?.txInfo?.view?;
 	// react-json-view was unhappy with the view object directly, and complained
 	// it didn't know how to JSONify an internal bigint, so just do an extra round trip to JSON
-	const rawTx = JSON.parse(tx?.txInfo?.transaction?.toJsonString() || "{}");
-	const rawView = JSON.parse(tx?.txInfo?.view?.toJsonString() || "{}");
+	const rawTx = JSON.parse(tx?.txInfo?.transaction?.toJsonString() || '{}')
+	const rawView = JSON.parse(tx?.txInfo?.view?.toJsonString() || '{}')
 
 	return (
 		<>
@@ -119,7 +119,7 @@ export default function TransactionDetail() {
 									<span>(Height {Number(tx?.txInfo?.height)})</span>
 								</div>
 								<p className='h2 mb-[12px] mt-[16px]'>Memo</p>
-								{ /* TODO: replace with a MemoViewComponent */}
+								{/* TODO: replace with a MemoViewComponent */}
 								<div className='flex flex-col p-[16px] gap-y-[16px] w-[800px] bg-brown rounded-[10px]'>
 									{memoText === 'Encrypted' ? (
 										<div className='w-[100%] flex flex-col'>
@@ -128,9 +128,6 @@ export default function TransactionDetail() {
 												<Copy text={memoSender} type='last' />
 											</div>
 										</div>
-									)}
-									{memoText === 'Encrypted' ? (
-										<EncryptedValue label='Message' />
 									) : (
 										<div className='w-[100%] flex flex-col'>
 											<p className='h3 mb-[8px] capitalize'>Message</p>
@@ -138,32 +135,39 @@ export default function TransactionDetail() {
 											This ensures an empty memo doesn't collapse the container but doesn't interact
 											with any of the other styling. it's very brittle but good enough for now
 											*/}
-											<p style={{ minHeight: '40px' }} className='py-[8px] px-[16px] bg-dark_grey rounded-[15px] text_numbers_s text-light_grey break-words '>
+											<p
+												style={{ minHeight: '40px' }}
+												className='py-[8px] px-[16px] bg-dark_grey rounded-[15px] text_numbers_s text-light_grey break-words '
+											>
 												{memoText}
 											</p>
 										</div>
 									)}
-									{
-										memoSender === 'Encrypted' ? (
-											<div className='w-[100%] flex flex-col'>
-												<p className='h3 mb-[8px] capitalize encrypted'>Return Address</p>
-											</div>
-										) : (
-											<div className='w-[100%] flex flex-col'>
-												<p className='h3 mb-[8px] capitalize'>Return Address</p>
-												<p className='py-[8px] px-[16px] bg-dark_grey rounded-[15px] text_numbers_s text-light_grey break-words monospace'>
-													<AddressComponent address={memoReturnAddress!} />
-												</p>
-											</div>
-										)
-									}
+									{memoSender === 'Encrypted' ? (
+										<div className='w-[100%] flex flex-col'>
+											<p className='h3 mb-[8px] capitalize encrypted'>
+												Return Address
+											</p>
+										</div>
+									) : (
+										<div className='w-[100%] flex flex-col'>
+											<p className='h3 mb-[8px] capitalize'>Return Address</p>
+											<p className='py-[8px] px-[16px] bg-dark_grey rounded-[15px] text_numbers_s text-light_grey break-words monospace'>
+												<AddressComponent address={memoReturnAddress!} />
+											</p>
+										</div>
+									)}
 								</div>
 								<p className='h2 mb-[12px] mt-[16px]'>Actions</p>
 								<div className='flex flex-col p-[16px] gap-y-[16px] w-[800px] bg-brown rounded-[10px]'>
-									{
-										tx.txInfo?.view?.bodyView?.actionViews.map((actionView, index) =>
-											<ActionViewComponent key={index} actionView={actionView} />
-										)}
+									{tx.txInfo?.view?.bodyView?.actionViews.map(
+										(actionView, index) => (
+											<ActionViewComponent
+												key={index}
+												actionView={actionView}
+											/>
+										)
+									)}
 								</div>
 								{/* TODO: replace by a TransactionDataComponent with everything other than Memo + Actions */}
 								<p className='h2 mb-[12px] mt-[16px]'>Transaction Data</p>
@@ -190,27 +194,35 @@ export default function TransactionDetail() {
 								<p className='h2 mb-[12px] mt-[16px]'>Raw Transaction</p>
 								<div className='flex flex-col p-[16px] gap-y-[16px] w-[800px] bg-brown rounded-[10px]'>
 									<div style={{ overflowX: 'auto', whiteSpace: 'nowrap' }}>
-										<DynamicReactJson src={rawTx}
-											theme="twilight"
+										<DynamicReactJson
+											src={rawTx}
+											theme='twilight'
 											name={false}
 											displayDataTypes={false}
 											collapseStringsAfterLength={48}
 											collapsed={true}
-											style={{ fontFamily: "'Iosevka', 'Menlo', 'Courier New', Courier, monospace" }}
+											style={{
+												fontFamily:
+													"'Iosevka', 'Menlo', 'Courier New', Courier, monospace",
+											}}
 										/>
 									</div>
 								</div>
 								<p className='h2 mb-[12px] mt-[16px]'>Raw View</p>
 								<div className='flex flex-col p-[16px] gap-y-[16px] w-[800px] bg-brown rounded-[10px]'>
 									<div style={{ overflowX: 'auto', whiteSpace: 'nowrap' }}>
-										<DynamicReactJson src={rawView}
-											theme="twilight"
+										<DynamicReactJson
+											src={rawView}
+											theme='twilight'
 											name={false}
 											displayDataTypes={false}
 											collapseStringsAfterLength={48}
 											collapsed={true}
 											/* HACK: the component adds inline styles that don't seem easily overridden by normal CSS rules, replicate .monospace here */
-											style={{ fontFamily: "'Iosevka', 'Menlo', 'Courier New', Courier, monospace" }}
+											style={{
+												fontFamily:
+													"'Iosevka', 'Menlo', 'Courier New', Courier, monospace",
+											}}
 										/>
 									</div>
 								</div>
--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -10,43 +10,44 @@ export const ActivityList = () => {
 	const { transactions } = useTransactions()
 
 	return (
-		<>
-			<div className='w-[100%] flex flex-col items-center'>
-				<div className='w-[100%] flex flex-col ext:mb-[32px] tablet:mb-[8px]'>
-					{transactions.map(i => {
-						return (
-							<div
-								key={Number(i.txInfo?.height)}
-								className='px-[18px] py-[12px] border-y-[1px] border-solid border-dark_grey ext:mb-[8px] tablet:mb-[16px flex justify-between items-center'
-							>
-								<div className='flex items-center w-[100%]'>
-									<ArrowUpRightSvg />
-									<div className='flex flex-col ml-[14px]'>
-										<p className='h3 w-[80px] mb-[8px]'>
-											{getTransactionType(i.txInfo?.view)}
-										</p>
-										<p className='text_body text-green'>Height</p>
-										<p className='text_body text-green'>
-											{String(i.txInfo?.height)}
-										</p>
-									</div>
-									<p className='text_body ml-[14px]'>
-										<TransactionHashComponent short_form={false} hash={uint8ToBase64(i.txInfo?.id?.hash!)} />
+		<div className='w-[100%] flex flex-col items-center'>
+			<div className='w-[100%] flex flex-col ext:mb-[32px] tablet:mb-[8px]'>
+				{transactions.map(i => {
+					return (
+						<div
+							key={Number(i.txInfo?.height)}
+							className='px-[18px] py-[12px] border-y-[1px] border-solid border-dark_grey ext:mb-[8px] tablet:mb-[16px flex justify-between items-center'
+						>
+							<div className='flex items-center w-[100%]'>
+								<ArrowUpRightSvg />
+								<div className='flex flex-col ml-[14px]'>
+									<p className='h3 w-[80px] mb-[8px]'>
+										{getTransactionType(i.txInfo?.view)}
+									</p>
+									<p className='text_body text-green'>Height</p>
+									<p className='text_body text-green'>
+										{String(i.txInfo?.height)}
 									</p>
 								</div>
+								<p className='text_body ml-[14px]'>
+									<TransactionHashComponent
+										short_form={false}
+										hash={uint8ToBase64(i.txInfo?.id?.hash!)}
+									/>
+								</p>
 							</div>
+							<Link
+								href={`${routesPath.TRANSACTION}?hash=${uint8ToBase64(
+									i.txInfo?.id?.hash!
+								)}`}
+								className='rotate-180 cursor-pointer'
+							>
+								<ChevronLeftIcon />
+							</Link>
 						</div>
-						<Link
-							href={`${routesPath.TRANSACTION}?hash=${uint8ToBase64(
-								i.txInfo?.id?.hash!
-							)}`}
-							className='rotate-180 cursor-pointer'
-						>
-							<ChevronLeftIcon />
-						</Link>
-					</div>
-				)
-			})}
+					)
+				})}
+			</div>
 		</div>
 	)
 }
